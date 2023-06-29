2bbb23180ba89295ad3dffa31447ee9a41cd0deb
Vladeson <123668587+Vladeson@users.noreply.github.com>
2023-06-29 21:42:56 +0000

    Deploying to gh-pages from @ Vladeson/goit-js-hw-09@db075f7ec4e9b1fa689125a0ecfb26e63dcebe33 🚀

---
+++ 01-color-switcher.8a49b15a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");let r=null;const d=()=>{r=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)};t.addEventListener("click",(()=>{d(),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(r),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.8a49b15a.js.map
